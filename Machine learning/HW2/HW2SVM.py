680a66df1d9c67783d3728d88c8c7a01eab32b54
ordinary813 <ordinaryperson813@gmail.com>
2024-02-10 10:04:07 +0200

    First implementation of svm

--- Machine learning/HW2/HW2SVM.py
+++ Machine learning/HW2/HW2SVM.py
@@ -20,3 +20,59 @@ X_train = data_dict['X_train']
 y_train = data_dict['y_train']
 X_val = data_dict['X_val']
 y_val = data_dict['y_val']
+
+new_features = []
+
+for row in X_train:
+   squaredValues = [val ** 2 for val in row]
+   squaredValues.extend(row)
+   new_features.append(squaredValues)
+
+model = SVC(kernel='linear', C=10)
+model.fit(new_features, y_train)
+
+# Get the hyperplane equation coefficients and intercept
+coefficients = model.coef_[0]
+intercept = model.intercept_
+
+# Print the hyperplane equation
+equation_parts = []
+for i in range(len(coefficients)):
+    equation_parts.append(f"({coefficients[i]:.3f} * X{i+1})")
+equation = " + ".join(equation_parts) + f" + ({intercept[0]:.3f})"
+
+print("Hyperplane equation:")
+print(f"  {equation}")
+
+train_features = 
+train_preds = 
+train_acc = # Implement here
+
+val_features = # Implement here
+val_preds = # Implement here
+val_acc = # Implement here
+
+xx, yy = np.meshgrid(np.arange(-2, 2.2, 0.1), np.arange(-2, 2.2, 0.1))
+data = np.c_[xx.ravel(), yy.ravel()]
+
+new_features = # Implement here the new features on 'data'
+Z = # Implement here the predictions of data into 2 classes, using w, b you found
+Z = Z.reshape(xx.shape)
+
+fig, axs = plt.subplots(1, 2, figsize=(12, 4))
+
+# Plot the training data on the first subplot
+axs[0].contourf(xx, yy, Z, alpha=0.8)
+scatter1 = axs[0].scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap='bwr')
+axs[0].set_xlabel('X')
+axs[0].set_ylabel('Y')
+axs[0].set_title(f'Train dataset - {train_acc:.4f} accuracy')
+
+# Plot the validation data on the second subplot
+axs[1].contourf(xx, yy, Z, alpha=0.8)
+scatter2 = axs[1].scatter(X_val[:, 0], X_val[:, 1], c=y_val, cmap='bwr')
+axs[1].set_xlabel('X')
+axs[1].set_ylabel('Y')
+axs[1].set_title(f'Validation dataset - {val_acc:.4f} accuracy')
+
+plt.show()
