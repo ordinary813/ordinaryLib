93ca4ed0f24a8ba78e9035b64d4ce39feda1ebd4
ordinary813 <>
2024-10-01 01:05:33 +0300

    Fixed country labels 1. Need to add more data to some countries

--- ProjectCS/ImgClassificationTest/createCSVLabels.py
+++ ProjectCS/ImgClassificationTest/createCSVLabels.py
@@ -8,27 +8,28 @@ from tqdm import tqdm
 geolocator = Nominatim(user_agent="geoapiExercises")
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
-csv_path = os.path.join(script_dir, 'coordinates.csv')
+csv_path = os.path.join(script_dir, 'coordinates_with_country.csv')
 df = pd.read_csv(csv_path)
 
 def get_country(row):
-    coord = f"{row['latitude']}, {row['longitude']}"
-    try:
-        # Perform reverse geocoding
-        location = geolocator.reverse(coord, exactly_one=True)
-        address = location.raw['address']
-        country = address.get('country', 'Unknown')  # Get the country, or 'Unknown' if not found
-        row['country'] = unidecode(country)
-    except Exception as e:
-        row['country'] = 'Unknown'
-        print(f"Error retrieving country for {coord}")
+    if row['country'] == 'Unknown':
+        coord = f"{row['latitude']}, {row['longitude']}"
+        try:
+            # Perform reverse geocoding
+            location = geolocator.reverse(coord, exactly_one=True)
+            address = location.raw['address']
+            country = address.get('country', 'Unknown')  # Get the country, or 'Unknown' if not found
+            row['country'] = unidecode(country)
+        except Exception as e:
+            row['country'] = 'Unknown'
+            print(f"\nError retrieving country for {coord}")
     return row
 
 tqdm.pandas(desc="Retrieving countries")
 
 df = df.progress_apply(get_country, axis=1)
-df['ID'] = range(len(df))
-df = df[['ID', 'latitude', 'longitude', 'country']]
-df.to_csv('coordinates_with_country.csv', index=False)
+# df['ID'] = range(len(df))
+# df = df[['ID', 'latitude', 'longitude', 'country']]
+df.to_csv('coordinates_with_country2.csv', index=False)
 
 print("New CSV with countries and ID column has been created.")
